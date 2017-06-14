ed571b8a430fa0b223e1dc720cb3ad6a13a89984
pvansanten <pvansanten@gmail.com>
2017-06-14 16:28:41 +0200

    Update command execution to have a configurable timeout after which the process is killed

--- example/example.go
+++ example/example.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"github.com/vansante/go-ffprobe"
 	"log"
+	"time"
 )
 
 func main() {
-	path := "D:/Downloads/big_buck_bunny.mp4"
+	path := "D:/Downloads/videorientation.mp4"
 
-	data, err := ffprobe.GetVideoData(path)
+	data, err := ffprobe.GetVideoData(path, 500 * time.Millisecond)
 	if err != nil {
 		log.Panicf("Error getting data: %v", err)
 	}
@@ -22,4 +23,13 @@ func main() {
 
 	log.Printf("\nDuration: %v\n", data.Format.Duration())
 	log.Printf("\nStartTime: %v\n", data.Format.StartTime())
+
+	//start := time.Now()
+	//for i := 0; i < 100; i++ {
+	//	_, err = ffprobe.GetVideoData(path)
+	//	if err != nil {
+	//		log.Panicf("Error getting data: %v", err)
+	//	}
+	//}
+	//log.Printf("100 times get time: %v", time.Now().Sub(start))
 }
--- ffprobe.go
+++ ffprobe.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"os/exec"
+	"time"
+	"bytes"
 )
 
 var ErrBinNotFound error = errors.New("ffprobe bin not found")
+var ErrTimeout error = errors.New("process timeout exceeded")
 
 var binPath string = "ffprobe"
 
@@ -14,7 +17,7 @@ func SetFFProbeBinPath(newBinPath string) {
 	binPath = newBinPath
 }
 
-func GetVideoData(filePath string) (data *ProbeData, err error) {
+func GetVideoData(filePath string, timeout time.Duration) (data *ProbeData, err error) {
 	cmd := exec.Command(
 		binPath,
 		"-v", "quiet",
@@ -23,8 +26,10 @@ func GetVideoData(filePath string) (data *ProbeData, err error) {
 		"-show_streams",
 		filePath,
 	)
+	var outputBuf bytes.Buffer
+	cmd.Stdout = &outputBuf
 
-	outputBuf, err := cmd.Output()
+	err = cmd.Start()
 	if err != nil {
 		if err == exec.ErrNotFound {
 			err = ErrBinNotFound
@@ -32,8 +37,26 @@ func GetVideoData(filePath string) (data *ProbeData, err error) {
 		return
 	}
 
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case <-time.After(timeout):
+		err = cmd.Process.Kill()
+		if err == nil {
+			err = ErrTimeout
+		}
+		return
+	case err = <-done:
+		if err != nil {
+			return
+		}
+	}
+
 	data = &ProbeData{}
-	err = json.Unmarshal(outputBuf, data)
+	err = json.Unmarshal(outputBuf.Bytes(), data)
 	if err != nil {
 		return
 	}
