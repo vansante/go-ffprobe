5f85d421f7be64c5ea30f61db5ff4db78088851c
Paul van Santen <pvansanten@gmail.com>
2020-04-27 12:15:17 +0200

    Move ffprobe test to root

--- ffprobe_test.go
+++ ffprobe_test.go
@@ -3,36 +3,34 @@ package ffprobe
 import (
 	"testing"
 	"time"
-
-	ffprobe "github.com/vansante/go-ffprobe"
 )
 
 func Test_ffprobe(t *testing.T) {
 	// test GetProbeData
-	path := "test.mp4"
-	data, err := ffprobe.GetProbeData(path, time.Second)
+	path := "assets/test.mp4"
+	data, err := GetProbeData(path, 3*time.Second)
 	if err != nil {
 		t.Errorf("Error getting data: %v", err)
 	}
 
 	// test ProbeData.GetStream
-	stream := data.GetStreams(ffprobe.StreamVideo)
+	stream := data.GetStreams(StreamVideo)
 	if len(stream) != 1 {
 		t.Errorf("It just has one video stream.")
 	}
 
-	stream = data.GetStreams(ffprobe.StreamAudio)
+	stream = data.GetStreams(StreamAudio)
 	if len(stream) != 1 {
 		t.Errorf("It just has one audio stream.")
 	}
 
 	// this stream is []
-	stream = data.GetStreams(ffprobe.StreamSubtitle)
+	stream = data.GetStreams(StreamSubtitle)
 	if len(stream) != 0 {
-		t.Errorf("it does not have subtitle stream.")
+		t.Errorf("It does not have a subtitle stream.")
 	}
 
-	stream = data.GetStreams(ffprobe.StreamAny)
+	stream = data.GetStreams(StreamAny)
 	if len(stream) != 2 {
 		t.Errorf("It should have two streams.")
 	}
