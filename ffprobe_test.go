96882aff9e8eadad4a29ea30dd878b653c9ce885
Paul van Santen <pvansanten@gmail.com>
2020-04-27 13:00:32 +0200

    Restructure tests to test more

--- ffprobe_test.go
+++ ffprobe_test.go
@@ -1,36 +1,90 @@
 package ffprobe
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
 	"testing"
 	"time"
 )
 
-func Test_ffprobe(t *testing.T) {
-	// test GetProbeData
-	path := "assets/test.mp4"
-	data, err := GetProbeData(path, 3*time.Second)
+const (
+	testPath = "assets/test.mp4"
+)
+
+func Test_ProbeURL(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelFn()
+
+	data, err := ProbeURL(ctx, testPath)
 	if err != nil {
 		t.Errorf("Error getting data: %v", err)
 	}
 
+	validateData(t, data)
+}
+
+func Test_ProbeURL_HTTP(t *testing.T) {
+	const testPort = 20811
+
+	ctx, cancelFn := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelFn()
+
+	// Serve all files in assets
+	go func() {
+		http.Handle("/", http.FileServer(http.Dir("./assets")))
+		err := http.ListenAndServe(fmt.Sprintf(":%d", testPort), nil)
+		t.Log(err)
+	}()
+
+	// Make sure HTTP is up
+	time.Sleep(time.Second)
+
+	data, err := ProbeURL(ctx, fmt.Sprintf("http://127.0.0.1:%d/test.mp4", testPort))
+	if err != nil {
+		t.Errorf("Error getting data: %v", err)
+	}
+
+	validateData(t, data)
+}
+
+func Test_ProbeReader(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelFn()
+
+	fileReader, err := os.Open(testPath)
+	if err != nil {
+		t.Errorf("Error opening test file: %v", err)
+	}
+
+	data, err := ProbeReader(ctx, fileReader)
+	if err != nil {
+		t.Errorf("Error getting data: %v", err)
+	}
+
+	validateData(t, data)
+}
+
+func validateData(t *testing.T, data *ProbeData) {
 	// test ProbeData.GetStream
-	stream := data.GetStreams(StreamVideo)
+	stream := data.StreamType(StreamVideo)
 	if len(stream) != 1 {
 		t.Errorf("It just has one video stream.")
 	}
 
-	stream = data.GetStreams(StreamAudio)
+	stream = data.StreamType(StreamAudio)
 	if len(stream) != 1 {
 		t.Errorf("It just has one audio stream.")
 	}
 
 	// this stream is []
-	stream = data.GetStreams(StreamSubtitle)
+	stream = data.StreamType(StreamSubtitle)
 	if len(stream) != 0 {
 		t.Errorf("It does not have a subtitle stream.")
 	}
 
-	stream = data.GetStreams(StreamAny)
+	stream = data.StreamType(StreamAny)
 	if len(stream) != 2 {
 		t.Errorf("It should have two streams.")
 	}
