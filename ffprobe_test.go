7a003aa52a7e1f2038d1f65ef28c6a42ca48dff5
Max Holland <max@livepeer.org>
2023-04-06 09:46:38 +0100

    Add test for side data

--- ffprobe_test.go
+++ ffprobe_test.go
@@ -137,3 +137,35 @@ func validateData(t *testing.T, data *ProbeData) {
 		t.Errorf("this video starts at 0s.")
 	}
 }
+
+func Test_ProbeSideData(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancelFn()
+
+	fileReader, err := os.Open("assets/test.mov")
+	if err != nil {
+		t.Errorf("Error opening test file: %v", err)
+	}
+
+	data, err := ProbeReader(ctx, fileReader)
+	if err != nil {
+		t.Errorf("Error getting data: %v", err)
+	}
+
+	videoStream := data.FirstVideoStream()
+	if videoStream == nil {
+		t.Error("Video Stream was nil")
+	}
+
+	if len(videoStream.SideDataList) < 1 {
+		t.Error("Side data list was empty")
+	}
+
+	rotation, err := videoStream.SideDataList[0].GetString("rotation")
+	if err != nil {
+		t.Errorf("Error getting rotation field: %v", err)
+	}
+	if rotation != "-180" {
+		t.Errorf("Expected rotation to be \"-180\", got \"%s\"", rotation)
+	}
+}
