b3a86aaebf064151f447a6cea1f55de361b0d7fd
Max Holland <max@livepeer.org>
2023-04-12 10:18:27 +0100

    add method doc

--- sidedata.go
+++ sidedata.go
@@ -10,6 +10,8 @@ var ErrSideDataNotFound = errors.New("side data not found")
 // SideDataList is a json data structure to represent stream side data
 type SideDataList []Tags
 
+// GetSideData searches the SideDataList for the given sideDataType and returns it if found.
+// ErrSideDataNotFound will be returned if side data with the given type can't be found.
 func (s *SideDataList) GetSideData(sideDataType string) (Tags, error) {
 	for _, sideData := range *s {
 		dataType, err := sideData.GetString(sideDataTypeField)
